eb51f007c24b56ae775675b68ee3e10cd8558c4f
Willy <wsquarepa@gmail.com>
2019-05-19 21:11:03 -0700

    Added time/date control to choose dates and times.

--- app/src/main/java/com/example/android/reminders/Reminder.java
+++ app/src/main/java/com/example/android/reminders/Reminder.java
@@ -3,9 +3,13 @@ package com.example.android.reminders;
 public class Reminder {
     private double latitude;
     private double longitude;
-    private long time;
     private String name;
     private String description;
+    private String startDate;
+    private String startTime;
+    private String endDate;
+    private String endTime;
+    private Boolean isTimeBased;
 
     public double getLatitude() {
         return latitude;
@@ -21,14 +25,6 @@ public class Reminder {
 
     public void setLongitude(double longitude) {
         this.longitude = longitude;
-    }
-
-    public long getTime() {
-        return time;
-    }
-
-    public void setTime(long time) {
-        this.time = time;
     }
 
     public String getName() {
@@ -47,14 +43,57 @@ public class Reminder {
         this.description = description;
     }
 
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public Boolean getTimeBased() {
+        return isTimeBased;
+    }
+
+    public void setTimeBased(Boolean timeBased) {
+        isTimeBased = timeBased;
+    }
+
     @Override
     public String toString() {
         return "Reminder{" +
                 "latitude=" + latitude +
                 ", longitude=" + longitude +
-                ", time=" + time +
                 ", name='" + name + '\'' +
                 ", description='" + description + '\'' +
+                ", startDate='" + startDate + '\'' +
+                ", startTime='" + startTime + '\'' +
+                ", endDate='" + endDate + '\'' +
+                ", endTime='" + endTime + '\'' +
                 '}';
     }
 }
--- app/src/main/java/com/example/android/reminders/ReminderSetup.java
+++ app/src/main/java/com/example/android/reminders/ReminderSetup.java
@@ -1,19 +1,27 @@
 package com.example.android.reminders;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 
+import java.text.DateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import static com.example.android.reminders.MainActivity.currentIndex;
@@ -24,7 +32,13 @@ public class ReminderSetup extends AppCompatActivity {
     // region - variables
     private EditText description;
     private EditText reminderName;
+    private EditText startDate;
+    private EditText startTime;
+    private EditText endDate;
+    private EditText endTime;
     //private EditText time;
+
+    private LinearLayout linearLayout;
 
     private CheckBox timeBased;
     private CheckBox locationBased;
@@ -53,13 +67,29 @@ public class ReminderSetup extends AppCompatActivity {
         collab = findViewById(R.id.collab);
         delete = findViewById(R.id.delete);
         setLocation = findViewById(R.id.currentLocation);
+        linearLayout = findViewById(R.id.timeBasedStuff);
+        startDate = findViewById(R.id.startDate);
+        startTime = findViewById(R.id.startTime);
+        endDate = findViewById(R.id.endDate);
+        endTime = findViewById(R.id.endTime);
 
         if (currentIndex > -1) {
             reminderName.setText(reminderList.get(currentIndex).getName());
             description.setText(reminderList.get(currentIndex).getDescription());
+            timeBased.setChecked(reminderList.get(currentIndex).getTimeBased());
+            if (reminderList.get(currentIndex).getTimeBased()) {
+                linearLayout.setVisibility(View.VISIBLE);
+                startDate.setText(reminderList.get(currentIndex).getStartDate());
+                startTime.setText(reminderList.get(currentIndex).getStartTime());
+                endDate.setText(reminderList.get(currentIndex).getEndDate());
+                endTime.setText(reminderList.get(currentIndex).getEndTime());
+            } else {
+                linearLayout.setVisibility(View.GONE);
+            }
             delete.setVisibility(View.VISIBLE);
         } else {
             delete.setVisibility(View.GONE);
+            linearLayout.setVisibility(View.GONE);
         }
 
         finishButton.setOnClickListener(new View.OnClickListener() {
@@ -75,44 +105,64 @@ public class ReminderSetup extends AppCompatActivity {
                 onDeleteClicked(null);
             }
         });
+
+        timeBased.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (timeBased.isChecked()) {
+                    linearLayout.setVisibility(View.VISIBLE);
+                } else {
+                    linearLayout.setVisibility(View.GONE);
+                }
+            }
+        });
+        startDate.setInputType(InputType.TYPE_NULL);
+        startDate.setOnClickListener(new EditTextOnClickListener(startDate, this, true));
+        startTime.setInputType(InputType.TYPE_NULL);
+        startTime.setOnClickListener(new EditTextOnClickListener(startTime, this, false));
+        endDate.setInputType(InputType.TYPE_NULL);
+        endDate.setOnClickListener(new EditTextOnClickListener(endDate, this, true));
+        endTime.setInputType(InputType.TYPE_NULL);
+        endTime.setOnClickListener(new EditTextOnClickListener(endTime, this, false));
     }
     public void onFinishClicked (View v)  {
 
-//        Reminder reminder;
-//        Date currentTime;
-//
-//        Log.d(String.valueOf(this), "Button clicked!");
-//        if (currentIndex == -1) {
-//            reminder = new Reminder();
-//            currentTime = new Date();
-//            reminder.setName(reminderName.getText().toString());
-//            reminder.setDescription(description.getText().toString());
-////            if (setLocation.isChecked()){
-////                reminder.setLatitude(location.getLatitude());
-////                reminder.setLongitude(location.getLongitude());
-////            } else{
-////                reminder.setLatitude(49.2051);
-////                reminder.setLongitude(-122.78);
-////            }
-//
-//            reminder.setLatitude(49.2051);
-//            reminder.setLongitude(-122.78);
-//
-//            reminder.setTime(currentTime.getTime());
-//
-//            reminderList.add(reminder);
-//        } else {
-//            reminder = reminderList.get(currentIndex);
-//            currentTime = new Date();
-//            reminder.setName(reminderName.getText().toString());
-//            reminder.setDescription(description.getText().toString());
-//            //reminder.setTime()
-//            reminder.setLatitude(49.2052);
-//            reminder.setLongitude(-122.78);
-//            reminder.setTime(currentTime.getTime());
-//        }
-//
-//        saveList();
+        Reminder reminder;
+
+        Log.d(String.valueOf(this), "Button clicked!");
+        if (currentIndex == -1) {
+            reminder = new Reminder();
+            reminder.setName(reminderName.getText().toString());
+            reminder.setDescription(description.getText().toString());
+            if (setLocation.isChecked()){
+                reminder.setLatitude(49.2051);
+                reminder.setLongitude(-122.78);
+            } else {
+                reminder.setLatitude(49.2051);
+                reminder.setLongitude(-122.78);
+            }
+
+            reminder.setLatitude(49.2051);
+            reminder.setLongitude(-122.78);
+            reminder.setStartDate(startDate.getText().toString());
+            reminder.setStartTime(startTime.getText().toString());
+            reminder.setEndDate(endDate.getText().toString());
+            reminder.setEndTime(endTime.getText().toString());
+            reminder.setTimeBased(timeBased.isChecked());
+
+            reminderList.add(reminder);
+        } else {
+            reminder = reminderList.get(currentIndex);
+            reminder.setName(reminderName.getText().toString());
+            reminder.setDescription(description.getText().toString());
+            reminder.setLatitude(49.2052);
+            reminder.setLongitude(-122.78);
+            reminder.setStartDate(startDate.getText().toString());
+            reminder.setStartTime(startTime.getText().toString());
+            reminder.setEndDate(endDate.getText().toString());
+            reminder.setEndTime(endTime.getText().toString());
+        }
+
+        saveList();
 
         //region - Commented URL items
 
@@ -132,14 +182,7 @@ public class ReminderSetup extends AppCompatActivity {
 //        }
         //endregion
 
-        LocationUtil.getLocation(this, new LocationUpdateListener() {
-            @Override
-            public void locationUpdated(Location location, Context context) {
-                Log.d("Location-test", "Updated lat: " + location.getLatitude() + " long: " + location.getLongitude());
-            }
-        });
-
-        //onBackPressed();
+        onBackPressed();
     }
 
     public void onDeleteClicked (View v) {
@@ -170,5 +213,50 @@ public class ReminderSetup extends AppCompatActivity {
         editor.putString("Reminder", myJSON);
         editor.apply();
         Toast.makeText(ReminderSetup.this,"Saved.",Toast.LENGTH_SHORT).show();
+    }
+
+    class EditTextOnClickListener implements View.OnClickListener {
+
+        EditText editText;
+        DatePickerDialog datePicker;
+        TimePickerDialog timePicker;
+        Context context;
+        Boolean isDate;
+        Calendar cldr;
+
+        public EditTextOnClickListener(EditText editText, Context context, Boolean isDate) {
+            this.editText = editText;
+            this.context = context;
+            this.isDate = isDate;
+            this.cldr = Calendar.getInstance();
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (isDate) {
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                datePicker = new DatePickerDialog(context,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                editText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                datePicker.show();
+            } else {
+                int hour = cldr.get(Calendar.HOUR_OF_DAY);
+                int minute = cldr.get(Calendar.MINUTE);
+                timePicker = new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+                     @Override
+                     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                         editText.setText("" + hourOfDay + ':' + minute);
+                     }
+                 },hour, minute, true);
+                timePicker.show();
+            }
+        }
     }
 }
